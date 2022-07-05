4b0536a7050960d8ddb5f25a522a2800a9fac652
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-05 15:55:41 -0500

    more generic future proof argument name

--- src/services/DatabaseService.ts
+++ src/services/DatabaseService.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js"
 import { SUPABASE_KEY, SUPABASE_URL } from "../constants/Environment"
 
 interface Database {
-  getAll: <Data>(table: string) => Promise<Data[] | undefined>
+  getAll: <Data>(route: string) => Promise<Data[] | undefined>
   getById: <Data extends { id: string }>(
     table: string,
     id: string,
@@ -11,14 +11,14 @@ interface Database {
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
-const getAll: Database["getAll"] = async (table) => {
-  const { data } = await supabase.from(table).select("*")
+const getAll: Database["getAll"] = async (route) => {
+  const { data } = await supabase.from(route).select("*")
   return data ?? undefined
 }
 
-const getById: Database["getById"] = async (table, id) => {
+const getById: Database["getById"] = async (route, id) => {
   const { data } = await supabase
-    .from(table)
+    .from(route)
     .select("*")
     .eq("id", id)
     .limit(1)
