74b6aa76c0a669f3d40ea6992166369a8ad13638
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-05 16:00:28 -0500

    added goBack

--- src/pages/ProjectDetail/ProjectDetail.tsx
+++ src/pages/ProjectDetail/ProjectDetail.tsx
@@ -1,13 +1,23 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { useGetProjectById } from "../../hooks/useGetProjectById"
 
 export function ProjectDetail() {
   const { id } = useParams<{ id: string }>()
   const { data, isLoading } = useGetProjectById(id ?? "")
+  const navigate = useNavigate()
+
+  const goBack = () => {
+    navigate(-1)
+  }
 
   if (isLoading) {
     return <p>Loading...</p>
   }
 
-  return <div>{data?.name}</div>
+  return (
+    <div className="flex flex-col">
+      <button onClick={goBack}>Go Back</button>
+      <p>{data?.name}</p>
+    </div>
+  )
 }
