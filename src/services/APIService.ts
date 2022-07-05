8953fa6eb0a8345d5772ebc488394ad8a3294205
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-05 15:24:40 -0500

    updated queries

--- src/services/APIService.ts
+++ src/services/APIService.ts
@@ -4,19 +4,26 @@ import { SUPABASE_KEY, SUPABASE_URL } from "../constants/Environment"
 
 interface API {
   getProjects: () => Promise<Project[] | undefined>
-  getProjectById: (id: string) => Promise<Project | undefined>
+  getProjectById: (id: Project["id"]) => Promise<Project | undefined>
 }
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
 const getProjects: API["getProjects"] = async () => {
   const { data } = await supabase.from<Project>("Projects").select("*")
+
   return data ?? undefined
 }
 
 const getProjectById: API["getProjectById"] = async (id) => {
-  const { data } = await supabase.from<Project>("Projects").select(id)
-  return data?.[0]
+  const { data } = await supabase
+    .from<Project>("Projects")
+    .select("*")
+    .eq("id", id)
+    .limit(1)
+    .single()
+
+  return data ?? undefined
 }
 
 export const APIService: API = {
