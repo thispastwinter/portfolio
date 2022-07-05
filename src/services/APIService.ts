48ddefb125e41c9fe8c1f649b1d92a0c2f6da647
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-05 14:40:15 -0500

    added project querys to service

--- src/services/APIService.ts
+++ src/services/APIService.ts
@@ -1,13 +1,25 @@
+import { createClient } from "@supabase/supabase-js"
 import { Project } from "../types/Project"
+import { SUPABASE_KEY, SUPABASE_URL } from "../constants/Environment"
 
 interface API {
-  getProjects: () => Promise<Project[]>
+  getProjects: () => Promise<Project[] | undefined>
+  getProjectById: (id: string) => Promise<Project | undefined>
 }
 
-const getProjects: API["getProjects"] = () => {
-  return new Promise((resolve) => resolve([]))
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+
+const getProjects: API["getProjects"] = async () => {
+  const { data } = await supabase.from<Project>("Projects").select("*")
+  return data ?? undefined
+}
+
+const getProjectById: API["getProjectById"] = async (id) => {
+  const { data } = await supabase.from<Project>("Projects").select(id)
+  return data?.[0]
 }
 
 export const APIService: API = {
   getProjects,
+  getProjectById,
 }
