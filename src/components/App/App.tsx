6066152e9801a89f90c3c8628dad9f762858e4e9
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-05 14:38:48 -0500

    removed boilerplate, replaced with provider and page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,12 @@
-import logo from "../../assets/logo.svg"
-import "./App.styles.css"
+import { QueryClient, QueryClientProvider } from "react-query"
+import { ProjectPage } from "../ProjectPage"
+
+const client = new QueryClient()
 
 export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/components/App/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={client}>
+      <ProjectPage />
+    </QueryClientProvider>
   )
 }
