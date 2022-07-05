5cdd951d921daccf965abbd54465f5c29e0a9b20
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-05 15:22:59 -0500

    added initial routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,23 @@
 import { QueryClient, QueryClientProvider } from "react-query"
-import { ProjectPage } from "../ProjectPage"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { MainLayout } from "../../layouts/MainLayout"
+import { ProjectDetail } from "../../pages/ProjectDetail"
+import { Projects } from "../../pages/Projects"
 
 const client = new QueryClient()
 
 export function App() {
   return (
     <QueryClientProvider client={client}>
-      <ProjectPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Navigate to="/projects" replace />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/projects/:id" element={<ProjectDetail />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   )
 }
