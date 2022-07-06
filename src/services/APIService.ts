d051cdb4eaff20f5a4058742d1d3980058cedd06
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-06 14:37:01 -0500

    added relationship logic

--- src/services/APIService.ts
+++ src/services/APIService.ts
@@ -1,3 +1,4 @@
+import { Category } from "../types/Category"
 import { Project } from "../types/Project"
 import { DatabaseService } from "./DatabaseService"
 
@@ -7,11 +8,17 @@ interface API {
 }
 
 const getProjects: API["getProjects"] = () => {
-  return DatabaseService.getAll<Project>("projects")
+  return DatabaseService.getAll<Project, Category>("projects", {
+    name: "categories",
+    values: ["name", "id", "icon_name"],
+  })
 }
 
 const getProjectById: API["getProjectById"] = (id) => {
-  return DatabaseService.getById<Project>("projects", id)
+  return DatabaseService.getById<Project, Category>("projects", id, {
+    name: "categories",
+    values: ["name", "id", "icon_name"],
+  })
 }
 
 export const APIService: API = {
--- src/services/DatabaseService.ts
+++ src/services/DatabaseService.ts
@@ -1,22 +1,39 @@
 import { createClient } from "@supabase/supabase-js"
 import { SUPABASE_KEY, SUPABASE_URL } from "../constants/Environment"
 
-interface Database {
-  getAll: <Data>(route: string) => Promise<Data[] | undefined>
-  getById: <Data>(route: string, id: string) => Promise<Data | undefined>
+type RelationShip<Table> = {
+  name: string
+  values: Array<keyof Table>
 }
+
+interface Database {
+  getAll: <Data, Table = unknown>(
+    route: string,
+    relationship?: RelationShip<Table>,
+  ) => Promise<Data[] | undefined>
+  getById: <Data, Table = unknown>(
+    route: string,
+    id: string,
+    relationship?: RelationShip<Table>,
+  ) => Promise<Data | undefined>
+}
+
+const getRelationshipQuery = <Table>(relationship?: RelationShip<Table>) =>
+  `${relationship?.name}(${relationship?.values.join(",")})`
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
-const getAll: Database["getAll"] = async (route) => {
-  const { data } = await supabase.from(route).select("*")
+const getAll: Database["getAll"] = async (route, relationship) => {
+  const { data } = await supabase
+    .from(route)
+    .select(`*, ${getRelationshipQuery(relationship)})`)
   return data ?? undefined
 }
 
-const getById: Database["getById"] = async (route, id) => {
+const getById: Database["getById"] = async (route, id, relationship) => {
   const { data } = await supabase
     .from(route)
-    .select("*")
+    .select(`*, ${getRelationshipQuery(relationship)})`)
     .eq("id", id)
     .limit(1)
     .single()
