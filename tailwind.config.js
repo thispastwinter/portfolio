7c9224913b4fb3419b4d581a358582482466908e
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-06 14:35:06 -0500

    extended base theme with colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      display1: ["PatuaOne"],
+      display2: ["BebasNeue"],
+    },
+    extend: {
+      colors: {
+        darkBlueSky: "#93b7be",
+        mintCream: "#f1fffa",
+        paleSilver: "#d5c7bc",
+        deepTaupe: "#785964",
+        secondary: "#454545",
+        primary: "#31493c",
+        richBlack: "#001a23",
+        russianViolet: "#420039",
+      },
+    },
   },
-  plugins: [],
 }
