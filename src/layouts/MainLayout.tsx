d33920a4f870558084b1e8bb3d0db1d0f976337c
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-06 14:59:56 -0500

    replaced padding with margin, removed cypress

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import { Header } from "../components/Header"
 
 export function MainLayout() {
   return (
-    <div className="flex flex-col p-8">
+    <div className="flex flex-col m-8">
       <Header />
       <Outlet />
     </div>
