4b207b2684c49ae70b1159fa85aaec958ebfefe5
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-07 15:04:02 -0500

    updated Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,12 +6,19 @@ export function Header() {
 
   return (
     <div
-      className={classNames({ "invisible h-0": match }, "md:visible md:h-auto")}
+      className={classNames(
+        { "invisible h-0": match },
+        "md:visible md:h-auto md:mb-10",
+      )}
     >
       <div className="mb-8">
-        <div className="flex flex-wrap mb-6">
-          <p className="text-8xl font-display1 ml-4 mr-6">Justin</p>
-          <p className="text-8xl font-display1">Klaas</p>
+        <div className="flex flex-wrap mb-4 max-w-sm">
+          <p className="text-8xl font-display1 ml-1.5 text-slate-600 drop-shadow-lg">
+            justin
+          </p>
+          <p className="text-8xl font-display1 text-slate-800 drop-shadow-lg">
+            klaas
+          </p>
         </div>
         <p className="text-2xl font-light text-secondary mt-4">
           application developer specializing in React-Native, GraphQL, and
