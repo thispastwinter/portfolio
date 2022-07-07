0a7e338c48985b67ec0751e4d9333193e45504f2
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-07 09:36:37 -0500

    reworked project detail

--- src/pages/ProjectDetail/ProjectDetail.tsx
+++ src/pages/ProjectDetail/ProjectDetail.tsx
@@ -1,10 +1,15 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { Icon } from "../../components/Icon"
 import { useGetProjectById } from "../../hooks/useGetProjectById"
 
 export function ProjectDetail() {
   const { id } = useParams<{ id: string }>()
   const { data, isLoading } = useGetProjectById(id ?? "")
+  const navigate = useNavigate()
+
+  const goBack = () => {
+    navigate(-1)
+  }
 
   if (isLoading) {
     return <p>Loading...</p>
@@ -12,20 +17,28 @@ export function ProjectDetail() {
 
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col mb-6">
-        <p className="text-8xl md:text-4xl font-display2 mb-4">{data?.name}</p>
-        <div className="flex gap-2">
-          {data?.categories.map(({ icon_name }) => (
-            <Icon size={24} key={icon_name} name={icon_name} />
-          ))}
+      <button className="flex pb-4 items-center md:hidden" onClick={goBack}>
+        <Icon name="chevronLeft" size={32} />
+        <p className="text-xl">Projects</p>
+      </button>
+      <div className="flex flex-wrap md:flex-nowrap">
+        <img
+          className="border rounded-lg max-w-xs mr-4"
+          alt={data?.name}
+          src={data?.image}
+        />
+        <div className="flex flex-col">
+          <p className="text-8xl md:text-4xl font-display2 my-4 md:my-0 md:mb-4">
+            {data?.name}
+          </p>
+          <div className="flex gap-2">
+            {data?.categories.map(({ icon_name }) => (
+              <Icon size={24} key={icon_name} name={icon_name} />
+            ))}
+          </div>
+          <p className="mt-6">{data?.description}</p>
         </div>
       </div>
-      <img
-        className="border rounded-lg max-w-xs"
-        alt={data?.name}
-        src={data?.image}
-      />
-      <p className="mt-6">{data?.description}</p>
     </div>
   )
 }
