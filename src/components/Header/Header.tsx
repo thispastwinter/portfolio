4428d69f759df239e8ec13e2213da61c16c9d6e4
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-08 15:40:41 -0500

    added nav links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames"
 import { useMatch } from "react-router-dom"
+import { NavLink } from "../NavLink"
 
 export function Header() {
   const match = useMatch("/projects/:id")
@@ -8,10 +9,10 @@ export function Header() {
     <div
       className={classNames(
         { "invisible h-0": match },
-        "md:visible md:h-auto md:mb-10",
+        "md:visible md:h-auto md:mb-16",
       )}
     >
-      <div className="mb-8">
+      <div className="mb-4">
         <div className="flex flex-wrap mb-4 max-w-sm">
           <p className="text-8xl font-display1 ml-1.5 text-slate-600 drop-shadow-lg">
             justin
@@ -25,6 +26,16 @@ export function Header() {
           Typescript.
         </p>
       </div>
+      <nav className="invisible h-0 md:visible md:h-auto">
+        <div className="flex gap-10 font-medium text-slate-600 text-xl">
+          <NavLink to="projects">Recent Work</NavLink>
+          <NavLink to="about">About</NavLink>
+          <NavLink to="https://www.github.com/thispastwinter">Github</NavLink>
+          <NavLink to="https://www.linkedin.com/in/justin-klaas-33a04085/">
+            LinkedIn
+          </NavLink>
+        </div>
+      </nav>
     </div>
   )
 }
