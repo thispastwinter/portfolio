4bc570af5876d71df4f3b411c5bbb89654124b8b
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-11 11:16:38 -0500

    added conditional types

--- src/types/ForeignTableObject.ts
+++ src/types/ForeignTableObject.ts
@@ -3,7 +3,9 @@ import { ListItem } from "./ListItem"
 
 export type ForeignTableObject<Data extends AnyObject, ForeignTable> = {
   foreignTable: ForeignTable
-  fields:
-    | Array<keyof ListItem<Data[ForeignTable]>>
-    | Array<keyof Data[ForeignTable]>
+  fields: Array<
+    Data[ForeignTable] extends Array<unknown>
+      ? keyof ListItem<Data[ForeignTable]>
+      : keyof Data[ForeignTable]
+  >
 }
