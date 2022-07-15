9dc2199af96cdc0e71eae54fe5f71dc7451f5fa4
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-15 13:03:25 -0500

    remvoved nav link check from test

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -8,22 +8,17 @@ describe("Header", () => {
     const title2 = "klaas"
     const subTitle =
       "application developer specializing in React-Native, GraphQL, and Typescript."
-    const navLinkLabel = /navigate to/i
 
     // Act
     render(<Header />)
     const receivedTitle1 = screen.getByText(title1)
     const receivedTitle2 = screen.getByText(title2)
     const receivedSubTitle = screen.getByText(subTitle)
-    const receivedNavLinks = screen.getAllByLabelText(navLinkLabel, {
-      exact: false,
-    })
 
     // Assert
     expect(receivedTitle1).toBeInTheDocument()
     expect(receivedTitle2).toBeInTheDocument()
     expect(receivedSubTitle).toBeInTheDocument()
-    expect(receivedNavLinks).toHaveLength(4)
   })
   it("is hidden when isHidden is 'true'", () => {
     // Arrange
