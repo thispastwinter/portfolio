af17ee0bcc6ad2406bd3c4d9673b98f0acc03b9f
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-15 22:22:35 -0500

    made social links interactable

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react"
+import { URL } from "../../constants/URL"
 import { Card } from "../Card"
 import { Icon } from "../Icon"
 
@@ -14,8 +15,12 @@ export function Footer({ children }: FooterProps) {
       <div className="flex items-center justify-between border-b-2 pb-10 mb-10">
         <p className="text-2xl font-medium">Get to know me</p>
         <div className="flex gap-x-4">
-          <Icon name="github" size={ICON_SIZE} />
-          <Icon name="linkedin" size={ICON_SIZE} />
+          <a target="_blank" href={URL.github} rel="noreferrer">
+            <Icon name="github" size={ICON_SIZE} />
+          </a>
+          <a target="_blank" href={URL.linkedIn} rel="noreferrer">
+            <Icon name="linkedin" size={ICON_SIZE} />
+          </a>
         </div>
       </div>
       <div className="flex flex-col max-w-6xl">{children}</div>
