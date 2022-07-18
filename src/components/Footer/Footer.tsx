d3901c4d1c1dde16935f529094faa0ce76647bd3
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-18 14:42:16 -0500

    replaced anchors with Button

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react"
 import { URL } from "../../constants/URL"
+import { Button } from "../Button"
 import { Card } from "../Card"
 import { Icon } from "../Icon"
 
@@ -11,26 +12,20 @@ interface FooterProps {
 
 export function Footer({ children }: FooterProps) {
   return (
-    <Card className="mt-10 md:mt-16 px-10 md:px-20 bg-transparent border border-[#222222] py-10 mb-10 text-[#222222]">
+    <Card className="mt-10 md:mt-16 px-10 md:px-20 bg-transparent border border-charcoal py-10 mb-10 text-charcoal">
       <div className="flex items-center justify-between border-b-2 pb-10 mb-10">
         <p className="text-2xl font-medium">Get to know me</p>
         <div className="flex gap-x-4">
-          <a
-            className="hover:opacity-60"
-            target="_blank"
-            href={URL.github}
-            rel="noreferrer"
-          >
-            <Icon name="github" size={ICON_SIZE} />
-          </a>
-          <a
-            className="hover:opacity-60"
-            target="_blank"
-            href={URL.linkedIn}
-            rel="noreferrer"
-          >
-            <Icon name="linkedin" size={ICON_SIZE} />
-          </a>
+          <Button
+            component="a"
+            to={URL.github}
+            startIcon={<Icon name="github" size={ICON_SIZE} />}
+          />
+          <Button
+            component="a"
+            to={URL.linkedIn}
+            startIcon={<Icon name="linkedin" size={ICON_SIZE} />}
+          />
         </div>
       </div>
       <div className="flex flex-col max-w-6xl">{children}</div>
