0097baf0b38c90b69477adb71e8da27f7f81c3bd
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-18 14:37:56 -0500

    updated Button to support link type, updated Button stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -15,9 +15,41 @@ export default {
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
 
-export const Example = Template.bind({})
+export const ButtonVariant = Template.bind({})
 
-Example.args = {
+ButtonVariant.args = {
+  component: "button",
   text: "Next project",
-  rightAdornment: <Icon className="ml-2" name="arrowRight" />,
+}
+
+export const LinkVariantDefault = Template.bind({})
+
+LinkVariantDefault.args = {
+  text: "Website",
+  component: "a",
+  to: "https://www.google.com",
+}
+
+export const LinkVariantCustom = Template.bind({})
+
+LinkVariantCustom.args = {
+  text: "Website",
+  component: "a",
+  to: "https://www.google.com",
+  endIcon: <Icon className="ml-2" name="globe" />,
+}
+
+export const EndIcon = Template.bind({})
+
+EndIcon.args = {
+  component: "button",
+  endIcon: <Icon className="ml-2" name="github" />,
+  text: "Github",
+}
+
+export const StartIcon = Template.bind({})
+
+StartIcon.args = {
+  startIcon: <Icon className="mr-2" name="linkedin" />,
+  text: "Linked In",
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,29 +1,79 @@
 import classNames from "classnames"
 import { ComponentProps, ReactNode } from "react"
+import { Icon } from "../Icon"
 
-export interface ButtonProps extends ComponentProps<"button"> {
-  text: string
-  leftAdornment?: ReactNode
-  rightAdornment?: ReactNode
+interface DefaultProps {
+  text?: string
+  startIcon?: ReactNode
+  endIcon?: ReactNode
+  className?: string
+  testId?: string
 }
 
-export function Button({
-  text,
-  leftAdornment,
-  rightAdornment,
-  ...rest
-}: ButtonProps) {
-  return (
-    <button
-      {...rest}
-      className={classNames(
-        "flex items-center mt-4 border p-4 border-gray-400 text-gray-700 ease-in-out duration-300 hover:border-gray-800 hover:bg-gray-800 hover:text-gray-50",
-        rest.className,
-      )}
-    >
-      {leftAdornment}
-      {text}
-      {rightAdornment}
-    </button>
+interface LinkProps extends DefaultProps {
+  component: "a"
+  to: string
+}
+
+export interface ButtonProps extends DefaultProps {
+  component: "button"
+  onClick: ComponentProps<"button">["onClick"]
+}
+
+type Props = ButtonProps | LinkProps
+
+export function Button(props: Props) {
+  const className = classNames(
+    {
+      "flex items-center mt-4 border p-4 border-gray-400 text-gray-700 ease-in-out duration-300 hover:border-gray-800 hover:bg-gray-800 hover:text-gray-50":
+        props.component === "button",
+    },
+    {
+      "flex items-center font-bold hover:opacity-60": props.component === "a",
+    },
+    props.className,
   )
+
+  if (props.component === "button") {
+    return (
+      <button
+        data-testid={props.testId}
+        onClick={props.onClick}
+        className={className}
+      >
+        {props.startIcon}
+        {props.text}
+        {props.endIcon}
+      </button>
+    )
+  } else {
+    const ariaLabel = `navigate to ${props.to}`
+    const anchorPropsType = props.to?.includes("http") ? "external" : "internal"
+    const defaultAnchorProps = {
+      href: props.to,
+      "aria-label": ariaLabel,
+    }
+    const anchorProps = {
+      external: {
+        ...defaultAnchorProps,
+        target: "_blank",
+        rel: "noreferrer",
+      },
+      internal: { ...defaultAnchorProps },
+    }
+
+    return (
+      <a
+        data-testid={props.testId}
+        {...anchorProps[anchorPropsType]}
+        className={className}
+      >
+        {props.startIcon}
+        {props.text}
+        {props.endIcon || (
+          <Icon size={20} className="ml-1" name="arrowUpRight" />
+        )}
+      </a>
+    )
+  }
 }
