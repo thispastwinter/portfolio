384966ba70c05d4f6916a889d7206c352167a622
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-18 14:38:48 -0500

    replace with button of 'a' type

--- src/components/ContentBlock/URLBlock.tsx
+++ src/components/ContentBlock/URLBlock.tsx
@@ -1,21 +1,14 @@
-import classNames from "classnames"
 import { ContentBlockProps } from "../../types/ContentBlockProps"
-import { Icon } from "../Icon"
+import { Button } from "../Button"
 
 export function URLBlock(props: Omit<ContentBlockProps, "type">) {
   return (
-    <a
-      data-testid={props.data_testid}
-      className={classNames(
-        "flex items-center font-bold hover:opacity-60",
-        props.className,
-      )}
-      target="_blank"
-      href={props.value}
-      rel="noreferrer"
-    >
-      {props.display_value}
-      <Icon size={20} className="ml-1" name="arrowUpRight" />
-    </a>
+    <Button
+      component="a"
+      className={props.className}
+      testId={props.data_testid}
+      to={props.value}
+      text={props.display_value}
+    />
   )
 }
