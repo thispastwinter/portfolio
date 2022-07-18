6b5765534f4b7c09e1a765323850a890d8fd4f7f
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-18 14:42:48 -0500

    removed unused colors, added new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,8 @@ module.exports = {
     },
     extend: {
       colors: {
-        darkBlueSky: "#93b7be",
-        mintCream: "#f1fffa",
-        paleSilver: "#d5c7bc",
-        deepTaupe: "#785964",
-        secondary: "#454545",
-        primary: "#31493c",
-        richBlack: "#001a23",
-        russianViolet: "#420039",
+        charcoal: "#222222",
+        background: "#f6f3f3",
       },
     },
   },
